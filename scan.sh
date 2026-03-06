0e195b40922744b5d9c7be026fd405eee2b43a19
Yassine Cherkaoui <yassinecherkaoui822@gmail.com>
2026-03-06 11:30:23 +0000

    Ajout script scan Nmap version simple

---
+++ scan.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "============================"
+echo "	  OUTIL SIMPLE DE SCAN NMAP   "
+echo "============================="
+echo "Entrez l'adresse IP ou le site :"
+read cible
+
+echo ""
+echo "Choisissez le type de scan :"
+echo "1 - Scan rapide (100 ports)"
+echo "2 -Scan complet (1000 ports)"
+echo "3 - Détecttion des services"
+echo "4 - Scan avancé (os + scripts)"
+echo "5 - Ping scan (machine active)"
+echo ""
+
+read choice
+
+if [ "$choice" == "1" ]; then
+	echo "Scan rapide..."
+	nmap -F $cible
+elif [ "$choice" == "2" ]; then 
+	echo "Scan complet..."
+	nmap $cible
+elif [ "$choice" == "3" ]; then
+	echo "Détection des services..."
+	nmap  -sV $cible
+elif [ "$choice" == "4" ]; then
+	echo "Scan Avancé"
+	sudo nmap -A $cible
+elif [ "$choice" == "5" ]; then
+       echo "Ping scan..."
+	nmap -sn $cible
+
+else
+    echo "Choix invalide"
+fi
